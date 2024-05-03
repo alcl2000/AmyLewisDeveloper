c7c85b79ca2d89825f024215887461aa05e9500d
Amy Lewis <amy.555.lewis@gmail.com>
2024-05-03 13:01:07 +0100

    add: home page route

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,12 @@
-
-import 'styles/App.css';
+import './styles/App.css';
+import {Route, Routes} from "react-router-dom";
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+          <Route exact path='/' element={<HomePage />} />
+    </Routes>
   );
 }
 
---
+++ portfolio/src/pages/home/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return( <div>
+        <h1>Hello World!</h1>
+    </div>)
+}
+
+export default HomePage
