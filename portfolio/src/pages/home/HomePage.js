771c72bec6cf3d73feeab53db0f898a47988a2a5
Amy Lewis <amy.555.lewis@gmail.com>
2024-05-07 17:15:39 +0100

    add: bootstrap install

--- portfolio/src/pages/home/HomePage.js
+++ portfolio/src/pages/home/HomePage.js
@@ -4,7 +4,9 @@ import styles from "../../styles/HomePage.module.css"
 const HomePage = () => {
     return( 
         <section className={styles.Main}>
-            <Logo/>
+            <div className="p-t-2 pl-2">
+                <Logo/>
+            </div>
             <h1 className={styles.Text}>
                 Hello
             </h1>
